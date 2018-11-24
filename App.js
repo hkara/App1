aa911df9f29e8a2842b63ffc53deca38a5b0fde6
Husayn Kara <husayn.kara@gmail.com>
2018-11-24 10:03:28 -0500

    added files

---
+++ App.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera, Permissions } from 'expo';
+
+export default class CameraExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+        
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+   <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  takePicture = () => {
+                    if (this.camera) {
+                      this.camera.takePictureAsync({ onPictureSaved: this.onPictureSaved });
+                    }
+                  };
+                
+                  handleMountError = ({ message }) => console.error(message);
+                
+                  onPictureSaved = async photo => {
+                    await FileSystem.moveAsync({
+                      from: photo.uri,
+                      to: `${FileSystem.documentDirectory}photos/${Date.now()}.jpg`,
+                    });
+                    this.setState({ newPhotos: true });
+                  }
+
+                }}>
+
+                <Text
+                  style={{ fontSize: 18, marginTop: 10, color: 'red' }}>
+                  {' '}[CAPTURE_IMAGE]{' '}
+                </Text>
+              </TouchableOpacity>
+
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+}
